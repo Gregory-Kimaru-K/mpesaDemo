7dd0b6a84dd49bb8b926a01d1538cdcd5aa48155
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-07-04 08:12:25 +0300

    last nights work

--- mpesa_project/mpesa_project/generateAccessToken.py
+++ mpesa_project/mpesa_project/generateAccessToken.py
@@ -1,5 +1,10 @@
 import requests
 from django.http import JsonResponse
+from mpesa_project import settings
 
 def get_access_token(request):
-
+    consumer_key = settings.MPESA_CONSUMER_KEY
+    consumer_secret = settings.MPESA_CONSUMER_SECRET
+    api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    headers = {'Content-Type' : 'application/json'}
+    auth = (consumer_key, consumer_secret)
